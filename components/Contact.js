2b094608e1f0a08fea4be6cdc5abfe5a499f22b9
Touger Thao <tougerthao@gmail.com>
2022-05-26 15:12:19 -0500

    updated env stuff

--- components/Contact.js
+++ components/Contact.js
@@ -81,8 +81,8 @@ export default function Contactsection() {
 
     emailjs
       .sendForm(
-        "touger01thao",
-        "template_0gljdqe",
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
         reRef.current,
         process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
       )
