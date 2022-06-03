9874b92676a6231547668a6e68ca1091f6898a66
Touger Thao <tougerthao@gmail.com>
2022-06-03 11:52:26 -0500

    added google analytics

--- components/OrderButton.js
+++ components/OrderButton.js
@@ -1,27 +1,9 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import styled from "@emotion/styled";
-import Link from "@mui/material/Link";
 import * as Scroll from "react-scroll";
 
-/* const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-  borderRadius: "15px",
-}; */
-
 export default function OrderButton() {
-  /*   const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false); */
-
   const StyledButton = styled(Button)({
     backgroundColor: "white",
     color: "black",
